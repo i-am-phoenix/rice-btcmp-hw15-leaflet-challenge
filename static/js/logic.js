2b60d30580c433efcf7e068ca6a19b62ae6db029
i-am-phoenix <iryna.p.marchiano@gmail.com>
2021-08-09 20:54:45 -0500

    reshuffling content of logic.jsgit

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,25 +4,24 @@ usgs_url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // Set up map object
 var myMap = L.map("map", {
     center: [29.749907, -95.358421], // Houston :)
-    zoom: 16
+    zoom:5
     }
 )
 
 
-// Create the tile layer that will be the background of our map.
+// Create the tile layer that will be the background of our map and add to myMap
 var bgLayer = L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
     }
-);
+).addTo(myMap);
 
-
+// ----------------------------------------------- FILTER/CONTROL SETUP ---------------------------------------------
 // Set up basemap variable
 var baseMap = {
     "Deafault" : bgLayer
 }
-
 L.control.layers(baseMap).addTo(myMap)
 
 
